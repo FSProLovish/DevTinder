2ccab7fb66ecd5bc2dbf855ea295e6be49acdc1a
FSProLovish <lavishgarg006@gmail.com>
2024-09-29 22:11:00 +0530

    Add basic get, post and delete route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "John",
+    lastName: "Doe",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("User has been created.");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User has been deleted.");
 });
 
 app.listen(7777, () => {
