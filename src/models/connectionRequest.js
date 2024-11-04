9d67eb9c7ad3f7d083efcfe8dcc13067915db5b8
FSProLovish <lavishgarg006@gmail.com>
2024-11-04 22:06:13 +0530

    Add sent connection request API

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const connectionRequestSchema = new Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["ignored", "rejected", "accepted", "interested"],
+        message: `{VALUE} is incorrect status type`,
+      },
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+
+module.exports = mongoose.model("ConnectionRequest", connectionRequestSchema);
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,59 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const router = express.Router();
 
-router.post("/sendConnectionRequest", userAuth, async (req, res) => {});
+router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    // validation for allowed statuses
+    const allowedStatuses = ["ignored", "interested"];
+    if (!allowedStatuses.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status type: " + status,
+      });
+    }
+
+    const toUser = await User.findById(toUserId);
+    if (!toUser) {
+      return res.status(404).json({
+        message: "User Not Found!",
+      });
+    }
+
+    // IF there is any existing `connection request`
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+
+    if (existingConnectionRequest) {
+      return res
+        .status(400)
+        .json({ message: "Connection Request Already Sent!!!" });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: req.user.firstName + " is " + status + " in " + toUser.firstName,
+      data,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 
 module.exports = router;
